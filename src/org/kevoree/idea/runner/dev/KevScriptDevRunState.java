aaa9be53088dab2a2b04771bc37ec1db29e5af4a
Francois Fouquet <fouquet.f@gmail.com>
2014-01-25 02:45:58 +0100

    cleanup bad commit with accidentaly remove Dev Runner with KevInf generation

--- src/org/kevoree/idea/runner/dev/KevScriptDevRunState.java
+++ src/org/kevoree/idea/runner/dev/KevScriptDevRunState.java
@@ -3,6 +3,7 @@ package org.kevoree.idea.runner.dev;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.JavaParameters;
 import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.openapi.compiler.CompilerPaths;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.roots.ModuleRootManager;
@@ -26,9 +27,7 @@ public class KevScriptDevRunState extends KevScriptRunState {
 
     @Override
     protected JavaParameters createJavaParameters() throws ExecutionException {
-
         KevScriptRunConfiguration runConfig = ((KevScriptRunConfiguration) getEnvironment().getRunnerAndConfigurationSettings().getConfiguration());
-
         Module module = runConfig.getConfigurationModule().getModule();
         JavaParameters parameters = new JavaParameters();
         ModuleRootManager moduleRootManager = ModuleRootManager.getInstance(module);
@@ -38,13 +37,22 @@ public class KevScriptDevRunState extends KevScriptRunState {
         if (kevoreeBase == null) {
             throw new ExecutionException("Unresolved Kevoree Runtime for version:" + runConfig.kevoreeRuntimeVersion);
         }
-        parameters.setJdk(SDK);
-        parameters.setMainClass("org.kevoree.platform.standalone.App");
+        //common params
         parameters.getClassPath().add(kevoreeBase);
         parameters.getVMParametersList().add("-Dnode.name=node0");
         parameters.getVMParametersList().add("-Dnode.bootstrap=" + runConfig.kevsFile.getPath());
         parameters.configureByModule(module, JavaParameters.CLASSES_ONLY);
+        parameters.setJdk(SDK);
 
+        File kevoreeAnnotator = KevoreeMavenResolver.resolve("org.kevoree.tools", "org.kevoree.tools.annotator.standalone", runConfig.kevoreeRuntimeVersion, "jar");
+        if (kevoreeAnnotator == null) {
+            parameters.setMainClass("org.kevoree.platform.standalone.App");
+        } else {
+            parameters.getClassPath().add(kevoreeAnnotator);
+            parameters.setMainClass("org.kevoree.tools.annotator.App");
+            parameters.getProgramParametersList().add(CompilerPaths.getModuleOutputDirectory(module, false).getPath());
+            parameters.getProgramParametersList().add("org.kevoree.platform.standalone.App");
+        }
         return parameters;
     }
 
