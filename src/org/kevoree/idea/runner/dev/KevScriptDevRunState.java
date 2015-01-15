c4e2bee62230ecb3e970ddbe2d264aa24795031e
Francois Fouquet <fouquet.f@gmail.com>
2015-01-15 20:27:58 +0100

    fix run mode

--- src/org/kevoree/idea/runner/dev/KevScriptDevRunState.java
+++ src/org/kevoree/idea/runner/dev/KevScriptDevRunState.java
@@ -1,7 +1,11 @@
 package org.kevoree.idea.runner.dev;
 
 import com.intellij.execution.ExecutionException;
+import com.intellij.execution.configurations.CommandLineBuilder;
+import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.execution.configurations.JavaCommandLineStateUtil;
 import com.intellij.execution.configurations.JavaParameters;
+import com.intellij.execution.process.OSProcessHandler;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.openapi.compiler.CompilerPaths;
 import com.intellij.openapi.module.Module;
@@ -10,7 +14,6 @@ import com.intellij.openapi.roots.ModuleRootManager;
 import org.kevoree.idea.runner.prod.KevScriptRunConfiguration;
 import org.kevoree.idea.runner.prod.KevScriptRunState;
 import org.kevoree.idea.utils.KevoreeMavenResolver;
-import org.kevoree.resolver.MavenResolver;
 
 import java.io.File;
 
@@ -22,42 +25,57 @@ public class KevScriptDevRunState extends KevScriptRunState {
     protected KevScriptDevRunState(ExecutionEnvironment environment) {
         super(environment);
     }
-
-    private static final MavenResolver resolver = new MavenResolver();
+    //private static final MavenResolver resolver = new MavenResolver();
 
     @Override
     protected JavaParameters createJavaParameters() throws ExecutionException {
         KevScriptRunConfiguration runConfig = ((KevScriptRunConfiguration) getEnvironment().getRunnerAndConfigurationSettings().getConfiguration());
         Module module = runConfig.getConfigurationModule().getModule();
-        JavaParameters parameters = new JavaParameters();
         ModuleRootManager moduleRootManager = ModuleRootManager.getInstance(module);
-        Sdk SDK = moduleRootManager.getSdk();
+        Sdk sdk = moduleRootManager.getSdk();
 
-        File kevoreeBase = KevoreeMavenResolver.resolve("org.kevoree.platform", "org.kevoree.platform.standalone", runConfig.kevoreeRuntimeVersion, "jar");
-        if (kevoreeBase == null) {
-            throw new ExecutionException("Unresolved Kevoree Runtime for version:" + runConfig.kevoreeRuntimeVersion);
+        File kevoreeAnnotator = KevoreeMavenResolver.resolve("org.kevoree.tools", "org.kevoree.tools.annotator", runConfig.kevoreeRuntimeVersion, "jar");
+        if (kevoreeAnnotator == null) {
+            throw new ExecutionException("Unresolved Kevoree Annotator for version:" + runConfig.kevoreeRuntimeVersion);
         }
-        //common params
+        JavaParameters paramAnnotator = new JavaParameters();
+        paramAnnotator.setUseDynamicClasspath(false);
+        paramAnnotator.setMainClass("org.kevoree.annotator.runner.App");
+        paramAnnotator.setWorkingDirectory(CompilerPaths.getModuleOutputDirectory(module, false).getPath());
+        paramAnnotator.getClassPath().add(kevoreeAnnotator);
+        paramAnnotator.getProgramParametersList().add(kevoreeAnnotator.getPath());
+        paramAnnotator.getVMParametersList().add("-Ddev.target.dirs=" + CompilerPaths.getModuleOutputDirectory(module, false).getPath());
+        paramAnnotator.configureByModule(module, JavaParameters.CLASSES_ONLY);
+        paramAnnotator.getVMParametersList().add("-Ddev.classloader=" + paramAnnotator.getClassPath().getPathsString().replace(kevoreeAnnotator.getPath(),""));
+        paramAnnotator.setJdk(sdk);
+        GeneralCommandLine gcmd = CommandLineBuilder.createFromJavaParameters(paramAnnotator, getEnvironment().getProject(), false);
+        OSProcessHandler handler = JavaCommandLineStateUtil.startProcess(gcmd, true);
+        handler.setShouldDestroyProcessRecursively(true);
+        try {
+            int res = handler.getProcess().waitFor();
+            File kevoreeBase = KevoreeMavenResolver.resolve("org.kevoree.platform", "org.kevoree.platform.standalone", runConfig.kevoreeRuntimeVersion, "jar");
+            if (kevoreeBase == null) {
+                throw new ExecutionException("Unresolved Kevoree Runtime for version:" + runConfig.kevoreeRuntimeVersion);
+            }
+            if (res == 0) {
+                //common params
+                JavaParameters parameters = new JavaParameters();
+                parameters.getClassPath().add(kevoreeBase);
+                parameters.getVMParametersList().add("-Dnode.name=" + runConfig.nodeName);
+                parameters.getVMParametersList().add("-Dnode.bootstrap=" + runConfig.kevsFile.getPath());
+                parameters.getVMParametersList().add("-Dversion=" + runConfig.kevoreeRuntimeVersion);
+                parameters.getVMParametersList().add("-Ddev.target.dirs=" + CompilerPaths.getModuleOutputDirectory(module, false).getPath());
+                parameters.setJdk(sdk);
+                parameters.setMainClass("org.kevoree.platform.standalone.App");
+                return parameters;
+            } else {
+                return null;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
 
-        parameters.getClassPath().add(kevoreeBase);
-
-        parameters.getVMParametersList().add("-Dkev.boot=dev");
-
-        parameters.getVMParametersList().add("-Dnode.name=" + runConfig.nodeName);
-        parameters.getVMParametersList().add("-Dnode.bootstrap=" + runConfig.kevsFile.getPath());
-        parameters.getVMParametersList().add("-Dversion=" + runConfig.kevoreeRuntimeVersion);
-
-        parameters.getVMParametersList().add("-Ddev.target.dirs=" + CompilerPaths.getModuleOutputDirectory(module, false).getPath());
-
-
-        JavaParameters parametersCL = new JavaParameters();
-        parametersCL.configureByModule(module, JavaParameters.CLASSES_ONLY);
-
-        parameters.getVMParametersList().add("-Ddev.classloader=" + parametersCL.getClassPath().getPathsString());
-
-        parameters.setJdk(SDK);
-        parameters.setMainClass("org.kevoree.platform.standalone.App");
-        return parameters;
     }
 
 }
